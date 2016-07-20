7847f0f0ff35df2e7d0326a4182b821fc6205ad2
ChengHsuanLiu <chenghsuango@gmail.com>
2016-07-20 22:59:29 +0800

    update

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,27 +1,40 @@
 $(document).ready(function(){
 	setTimeout(function(){
 		fade_action($('.navbar-brand'));
-	}, 200);
+	}, 100);
 
 	setTimeout(function(){
 		fade_action($('.navbar-nav'));
-	}, 600);
+	}, 400);
 
 	setTimeout(function(){
 		fade_action($('.mei-hua'));
-	}, 1000);
+	}, 700);
 
 	setTimeout(function(){
 		fade_action($('.ruby-1'));
-	}, 1800);
+	}, 1200);
 
 	setTimeout(function(){
 		fade_action($('.wrapper'));
-	}, 2000);
+	}, 1700);
+
+	setTimeout(function(){
+		fade_action($('.footer'));
+	}, 2200);
 
 	function fade_action(ele){
 		$(ele).fadeIn(1000);
 	};
+
+
+	$(window).scroll(function(){
+		if($(window).scrollTop() > 150){
+			$('.hide-navbar').addClass('in-show');
+		}else{
+			$('.hide-navbar').removeClass('in-show');
+		}
+	})
 })
 
 
